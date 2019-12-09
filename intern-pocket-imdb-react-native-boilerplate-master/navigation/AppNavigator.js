09ef9db481a212b3d06bdd416786a276bb27bf21
Milan Grozdanic <mgrozdanic333@gmail.com>
2019-12-09 11:02:26 +0100

    MovieNavigator file deleted as it was a mistake and no longer needed

--- intern-pocket-imdb-react-native-boilerplate-master/navigation/AppNavigator.js
+++ intern-pocket-imdb-react-native-boilerplate-master/navigation/AppNavigator.js
@@ -3,7 +3,6 @@ import { createAppContainer, createSwitchNavigator } from "react-navigation";
 import AuthLoading from "../screens/AuthLoading";
 import MainTabNavigator from "./MainTabNavigator";
 import AuthNavigator from "./AuthNavigator";
-import MovieNavigator from "./MovieNavigator";
 
 const AppNavigator = createSwitchNavigator(
   {
--- intern-pocket-imdb-react-native-boilerplate-master/navigation/MovieNavigator.js
+++
@@ -1,9 +0,0 @@
-import { createStackNavigator } from "react-navigation-stack";
-
-import MoviesItem from "../components/movies/MoviesItem";
-import MovieDetails from "../components/movies/MovieDetails";
-
-export default MovieNavigator =  createStackNavigator({
-    MoviesItem,
-    MovieDetails
-  });
